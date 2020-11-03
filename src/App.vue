1ba1d1715e0d928ce575637e3106a5d3c9dd74c1
ihsanait <ihsan@ait.com.tr>
2020-11-03 18:11:40 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Vue.js WebSocket</h2>
+    <h5 v-text="getData"></h5>
+    <input v-model="message" />
+    <button v-on:click="sendMessage()">Send Message</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      connection: null,
+      eventData: "DATA",
+      message: "",
+    };
+  },
+  created() {
+    this.connection = new WebSocket("ws://localhost:5000/testws");
+    this.connection.onmessage = function (event) {
+      console.log(this);
+      console.log(event);
+      console.log(event.data);
+    };
+  },
+  computed: {
+    getData() {
+      return this.eventData;
+    },
+  },
+  methods: {
+    sendMessage() {
+      this.connection.send(this.message);
+    },
+  },
+};
 </script>
 
 <style>
